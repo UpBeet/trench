a20b42838601a95508f27ff8d2545820971a4f72
Peter Gyory <ptgyory@gmail.com>
2017-11-12 11:27:45 -0700

    Add some initial aframe stuff

--- src/app/index.js
+++ src/app/index.js
@@ -2,9 +2,26 @@ import xs from 'xstream';
 import { section } from '@cycle/dom';
 import isolate from '@cycle/isolate';
 
+import { aScene, aEntity, aSky } from './utils/AframeHyperscript';
+
+const sky = aSky({ attrs: { color: '#f5f5f5' } });
+const sphere = aEntity(
+    {
+      attrs: {
+        geometry: 'primitive: sphere; radius: 0.05; segmentsWidth: 10; segmentsHeight: 10;',
+        material: 'flatShading: true; color: "#000000"',
+        position: '0 1.5 -1',
+      },
+    }
+  );
+
 function Trench(sources) {
     const { DOM, storage } = sources;
-    const vdom$ = xs.of(section('#game', 'Peter <3 Brax'));
+    const vdom$ = xs.of(
+        section(
+            '#game', [aScene([sky, sphere])]
+        )
+    );
     
     const sinks = {
         DOM: vdom$,
